c380eda417f843042b676abf6ed53cdfa2bac00f
fercamargo@dmx <fernando_camargo2017@outlook.com>
2023-08-31 16:04:12 -0300

    att: updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Appointment",
-  description: "Test",
+  description: "Tests",
 };
 
 export default function RootLayout({
